c3aaccd41321a57ec6145837035b8bb642098132
Timur Khamidov <baton237@gmail.com>
2022-12-24 16:11:41 +0300

    Add TCCancellable

---
+++ TCCancellable.swift
@@ -0,0 +1,10 @@
+/// A protocol indicating that an operation supports cancellation.
+public protocol TCCancellable {
+    
+    // MARK: - Methods
+    
+    /// Cancels the operation.
+    ///
+    /// Repeated calls of this method will be ignored.
+    func cancel()
+}
