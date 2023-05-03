92258a604f28d23514f7816347a3d78c60447b36
Aleksandr Darovskikh <ext.adarovskikh@tinkoff.ru>
2023-05-03 11:06:19 +0500

    Add tests for Test Task factory

---
+++ Tests/TinkoffConcurrencyTesting/TCTestTaskFactoryTests.swift
@@ -0,0 +1,84 @@
+import XCTest
+
+import TinkoffConcurrencyTesting
+
+final class TaskFactoryTests: XCTestCase {
+
+    // MARK: - Dependencies
+
+    private var taskFactory: TCTestTaskFactory!
+
+    // MARK: - XCTestCase
+
+    override func setUp() {
+        super.setUp()
+        taskFactory = TCTestTaskFactory()
+    }
+
+    override func tearDown() {
+        taskFactory = nil
+        super.tearDown()
+    }
+
+    // MARK: - Tests
+
+    func test_taskStarted_whenTaskCreated() async throws {
+        // given
+        let isTaskStarted = UncheckedSendable(false)
+        
+        // when
+        let task = taskFactory.task {
+            isTaskStarted.mutate { $0 = true }
+        }
+        _ = try await task.value
+        
+        // then
+        XCTAssertTrue(isTaskStarted.value)
+    }
+
+    func test_runUntilIdle_whenCreatedSeveralTasks() async throws {
+        // given
+        let tasksCount = 3
+        let finishedTasksCount = UncheckedSendable(0)
+
+        // when
+        for _ in 0..<tasksCount {
+            taskFactory.task {
+                finishedTasksCount.mutate { $0 += 1 }
+            }
+        }
+        try await taskFactory.runUntilIdle()
+        
+        // then
+        XCTAssertEqual(finishedTasksCount.value, tasksCount)
+    }
+    
+    func test_runUntilIdle_whenCreatedInternalTaskInRunningAsynchronousWork() async throws {
+        // given
+        let tasksCount = 2
+        let finishedTasksCount = UncheckedSendable(0)
+
+        // when
+        taskFactory.task {
+            self.taskFactory.task {
+                finishedTasksCount.mutate { $0 += 1 }
+            }
+            finishedTasksCount.mutate { $0 += 1 }
+        }
+        try await taskFactory.runUntilIdle()
+        
+        // then
+        XCTAssertEqual(finishedTasksCount.value, tasksCount)
+    }
+    
+    @MainActor func test_inheritedActorContext_whenTaskCreated() async throws {
+        // given
+        @MainActor func foo() {}
+
+        // when
+        taskFactory.task {
+            // then
+            foo()
+        }
+    }
+}
