ac55808a377723e4f714fd0658d28e595ad90b26
Aleksandr Darovskikh <ext.adarovskikh@tinkoff.ru>
2023-05-05 14:53:44 +0500

    Bring TaskFactory test code coverage to 100%

--- Tests/TinkoffConcurrency/TaskFactory/TCTaskFactoryTests.swift
+++ Tests/TinkoffConcurrency/TaskFactory/TCTaskFactoryTests.swift
@@ -8,6 +8,10 @@ final class TCTaskFactoryTests: XCTestCase {
 
     private var taskFactory: TCTaskFactory!
 
+    // MARK: - Private Static Properties
+
+    @TaskLocal private static var testValue = 42
+
     // MARK: - XCTestCase
 
     override func setUp() {
@@ -18,12 +22,16 @@ final class TCTaskFactoryTests: XCTestCase {
 
     // MARK: - Tests
 
-    func test_taskStarted_whenTaskCreated() async throws {
+    func test_task_whenTaskCreated() async throws {
         // given
         let result = Int.fake()
         
-        let task = taskFactory.task {
-            return result
+        let task = TCTaskFactoryTests.$testValue.withValue(999) {
+            taskFactory.task {
+                XCTAssertEqual(TCTaskFactoryTests.testValue, 999)
+
+                return result
+            }
         }
         
         // when
@@ -33,12 +41,54 @@ final class TCTaskFactoryTests: XCTestCase {
         XCTAssertEqual(taskResult, result)
     }
 
-    func test_runUntilIdle_whenCreatedTaskThrows() async throws {
+    func test_task_whenCreatedTaskThrows() async throws {
         // given
         let error = FakeErrors.default
 
-        let task = taskFactory.task {
-            throw error
+        let task = TCTaskFactoryTests.$testValue.withValue(999) {
+            taskFactory.task {
+                XCTAssertEqual(TCTaskFactoryTests.testValue, 999)
+
+                throw error
+            }
+        }
+
+        // when
+        let result = await XCTExecuteThrowsError(try await task.value)
+
+        // then
+        XCTAssertEqualErrors(result, error)
+    }
+
+    func test_detached_whenTaskCreated() async throws {
+        // given
+        let result = Int.fake()
+        
+        let task = TCTaskFactoryTests.$testValue.withValue(999) {
+            taskFactory.detached {
+                XCTAssertEqual(TCTaskFactoryTests.testValue, 42)
+                
+                return result
+            }
+        }
+        
+        // when
+        let taskResult = await task.value
+
+        // then
+        XCTAssertEqual(taskResult, result)
+    }
+
+    func test_detached_whenCreatedTaskThrows() async throws {
+        // given
+        let error = FakeErrors.default
+
+        let task = TCTaskFactoryTests.$testValue.withValue(999) {
+            taskFactory.detached {
+                XCTAssertEqual(TCTaskFactoryTests.testValue, 42)
+
+                throw error
+            }
         }
 
         // when
